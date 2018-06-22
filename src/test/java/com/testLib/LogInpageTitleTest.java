9143cfab4a7d85f21f08cdb1e9aef5a019415442
PARTHA <psjena07@gmail.com>
2018-06-22 18:40:58 +0530

    my commit

--- src/test/java/com/testLib/LogInpageTitleTest.java
+++ src/test/java/com/testLib/LogInpageTitleTest.java
@@ -8,20 +8,21 @@ import com.baseLib.BaseTest;
 import com.genericLib.ExtentReport;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class LogInpageTitleTest extends BaseTest{
-	Logger log = Logger.getLogger(LogInpageTitleTest.class);
+public class LogInpageTitleTest extends BaseTest
+{
+	//Logger log = Logger.getLogger(LogInpageTitleTest.class);
 	String title="Free CRM software in the cloud powers sales and customer service";
 	ExtentReport e=new ExtentReport();
 	@Test
 	public void loginPageTitleTest()
 	{
-		log.info("****************************** starting test case *****************************************");
+		//log.info("****************************** starting test case *****************************************");
 		
 		String actual=driver.getTitle();
 		Assert.assertEquals(actual, title,"title is not maching so fail");
 		
 		//e.extentTest.log(LogStatus.FAIL, "fail due to title is not matching");
-		 log.info("*************************end test*******************************");
+		// log.info("*************************end test*******************************");
 		// e.extentTest.log(LogStatus.PASS, "in login page");
 	}
 
